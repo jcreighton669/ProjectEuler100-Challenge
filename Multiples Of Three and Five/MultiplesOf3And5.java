1456cf70c7fc67f8aace09457abb3f18025e85db
Justin Allen Creighton <46484674+jcreighton669@users.noreply.github.com>
2020-01-03 23:22:41 -0500

    Update MultiplesOf3And5.java

--- Multiples Of Three and Five/MultiplesOf3And5.java
+++ Multiples Of Three and Five/MultiplesOf3And5.java
@@ -2,6 +2,13 @@ public class MultiplesOf3And5 {
     public static void main(String[] args) {
         System.out.println(multiplesOf3and5(10));
     }
+    /*
+    If we list all the natural numbers below 10 that are multiples of 3 or 5,
+    we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+    Find the sum of all the multiples of 3 or 5 below the provided parameter
+    value number.
+     */
     public static long multiplesOf3and5(int number) {
         long sum = 0;
         for (int i = 0; i < number; i++) {
