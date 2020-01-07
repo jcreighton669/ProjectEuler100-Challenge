11da6735b5da0c32f29b114b1ad282f8e624ac70
Justin Allen Creighton <46484674+jcreighton669@users.noreply.github.com>
2020-01-07 09:04:38 -0500

    Update SumDifference.java

--- Sum Square Difference/SumDifference.java
+++ Sum Square Difference/SumDifference.java
@@ -10,13 +10,13 @@ public class SumSquareDifference {
     /*
     The sum of the squares of the first ten natural numbers is,
     1^2 + 2^2 + ... + 10^2 = 385
-    
+
     The square of the sum of the first ten natural numbers is,
     (1 + 2 + ... + 10)^2 = 552 = 3025
-    
+
     Hence the difference between the sum of the squares of the first ten
     natural numbers and the square of the sum is 3025 − 385 = 2640.
-    
+
     Find the difference between the sum of the squares of the first one hundred
     natural numbers and the square of the sum.
      */
@@ -33,12 +33,12 @@ public class SumSquareDifference {
             sencondArray[i] = j * j;
         }
 
-        for (int i = 0; i < firstArray.length; i++) {
-            firstSum += firstArray[i];
+        for (int item : firstArray) {
+            firstSum += item;
         }
         int firstProduct = firstSum * firstSum;
-        for (int i = 0; i < sencondArray.length; i++) {
-            secondSum += sencondArray[i];
+        for (int value : sencondArray) {
+            secondSum += value;
         }
         System.out.println(firstProduct - secondSum);
     }
