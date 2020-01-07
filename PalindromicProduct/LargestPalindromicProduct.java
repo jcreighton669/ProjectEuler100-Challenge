9fa69b64dcf3620a7d7cd207e5fc0aaedcbc4e85
Justin Allen Creighton <46484674+jcreighton669@users.noreply.github.com>
2020-01-07 12:22:44 -0500

    Update LargestPalindromicProduct.java

--- PalindromicProduct/LargestPalindromicProduct.java
+++ PalindromicProduct/LargestPalindromicProduct.java
@@ -1,19 +1,27 @@
 import java.util.Scanner;
 
 public class LargestPalindromicProduct {
+    /*
+    A palindromic number reads the same both ways. The largest palindrome made
+    from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+    Find the largest palindrome made from the product of two 3-digit numbers.
+     */
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter number of digits for multiplier: ");
         int digits = scanner.nextInt();
-
+        int count = 0;
         int firstValue = 0;
         int secondValue = 0;
         int product = 0;
         int palindromicProduct = 0;
         for (int i = (int)Math.pow(10, digits-1); i < (int)Math.pow(10, digits); i++) {
-            for (int j = 100; j < 1000; j++) {
+            for (int j = (int)Math.pow(10, digits-1); j < (int)Math.pow(10, digits); j++) {
                 product = i * j;
-                if (isPalindrome(product) && product > palindromicProduct) {
+                if (isPalindrome(product)) {
+                    count++;
                     firstValue = i;
                     secondValue = j;
                     palindromicProduct = product;
@@ -21,6 +29,7 @@ public class LargestPalindromicProduct {
             }
         }
         System.out.println(firstValue + " * " + secondValue + " = " + palindromicProduct);
+        System.out.println(count);
     }
 
 
